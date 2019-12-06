9292d0e880df403ffea1ed7943705c86e335896f
mafalda-zuquete <55488606+mafalda-zuquete@users.noreply.github.com>
2019-12-06 09:30:24 +0000

    Cycle crossover
    A try for cycle crossover

--- cifo/algorithm/ga_operators.py
+++ cifo/algorithm/ga_operators.py
@@ -238,7 +238,43 @@ def pmx_crossover( problem, solution1, solution2):
 # -------------------------------------------------------------------------------------------------
 # TODO: implement Cycle Crossover
 def cycle_crossover( problem, solution1, solution2):
-    pass
+    cycles = []
+    considered = []
+
+    # finding the cycles
+    while len(considered) < len(solution1):
+        i = 1
+
+        while i in considered:
+            i += 1
+        
+        cycle =  []
+        full_cycle = False
+
+        while full_cycle == False:
+            cycle = cycle.append(i)
+            considered = considered.append(i)
+            i = solution1.index(solution2[i])
+
+            if i in considered:
+                full_cycle = True
+
+        cycles = cycles.append(cycle)
+    
+    child1 =  [None] * len(solution1)
+    child2 =  [None] * len(solution1)
+    
+    # getting the children
+    for i, cycle in enumerate(cycles):
+        # note that here cycle 1 is the cycle with index 0
+        if i % 2 == 0:
+            child1[i] = solution1[i]
+            child2[i] = solution2[i]
+        else:
+            child1[i] = solution2[i]
+            child2[i] = solution1[i]
+
+    return child1, child2
 
 ###################################################################################################
 # MUTATION APPROACHES
