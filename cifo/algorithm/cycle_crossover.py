4a0cca3a170dfed165bdafb1ff95ac63865174ec
Catarina Palha <catarina.palha@tekever.com>
2019-12-07 14:48:26 +0000

    Fixed some things on cycle cross over file.

--- cifo/algorithm/cycle_crossover.py
+++ cifo/algorithm/cycle_crossover.py
@@ -1,32 +1,57 @@
-parent1 = [8, 4, 7, 3, 6, 2, 5, 1, 9, 0]
-parent2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+def cycle_crossover(solution1, solution2):
 
-cycles = [-1]*len(parent1)
-cycle_no = 1
-cyclestart = (i for i,v in enumerate(cycles) if v < 0)
+    cycles = []
+    #
+    considered = []
+    
+    # finding the cycles
+    while len(considered) < len(solution1):
+        i = 1
 
-for pos in cyclestart:
+        while i in considered:
+            i += 1
+        
+        cycle =  []
+        full_cycle = False
 
-    while cycles[pos] < 0:
-        cycles[pos] = cycle_no
-        pos = parent1.index(parent2[pos])cycles = [-1]*len(parent1)
-cycle_no = 1
-cyclestart = (i for i,v in enumerate(cycles) if v < 0)
+        while full_cycle == False:
+            print(i)
+            #is not appending
+            cycle.append(i)
+            #is not appending
 
-for pos in cyclestart:
+            considered.append(i)
+            i = solution1.index(solution2[i])
 
-    while cycles[pos] < 0:
-        cycles[pos] = cycle_no
-        pos = parent1.index(parent2[pos])
+            if i in considered:
+                full_cycle = True
 
-    cycle_no += 1
+        cycles.append(cycle)
+    
+    child1 =  [None] * len(solution1)
+    child2 =  [None] * len(solution1)
+    
+    # getting the children
+    print(cycles)
+    for j in range(0, len(cycles)):
 
-child1 = [parent1[i] if n%2 else parent2[i] for i,n in enumerate(cycles)]
-child2 = [parent2[i] if n%2 else parent1[i] for i,n in enumerate(cycles)]
+        for i, cycle in enumerate(cycles):
+            # note that here cycle 1 is the cycle with index 0
+            if i % 2 == 0:
+                child1[i] = solution1[i]
+                child2[i] = solution2[i]
+            else:
+                child1[i] = solution2[i]
+                child2[i] = solution1[i]
+
+    return child1, child2
 
 
-print("parent1:", parent1)
-print("parent2:", parent2)
-print("cycles:", cycles)
-print("child1:", child1)
-print("child2:", child2)
+p1 = [8, 4, 7, 3, 6, 2, 5, 1 ,9, 0]
+p2 = [0, 1, 2, 3 ,4 ,5 ,6, 7, 8, 9]
+
+b = cycle_crossover(p1, p2)
+print(b)
+
+# Child 1:  8 1 2 3 4 5 6 7 9 0
+# Child 2:  0 4 7 3 6 2 5 1 8 9 
