f5ffc226739624f25afdf1ebd3d262f106621d02
mafalda-zuquete <55488606+mafalda-zuquete@users.noreply.github.com>
2019-12-08 15:18:21 +0000

    TSP is_admissible and build_solution
    is_admissible is done (to be tested) and a small edit was done in the testing data in build_solution

---
+++ cifo_project_v2/cifo_project/cifo/custom_problem/TSP_is_admissible.py
@@ -0,0 +1,19 @@
+def is_admissible( self, solution ): #<< use this signature in the sub classes, the meta-heuristic 
+    """
+    Check if the solution is admissible, considering the no cities can be repeated
+    """
+    counts = [solution.count(i) for i in solution]
+    
+    repeated = False
+    i = 0
+    
+    while repeated == False and i < len(counts):
+        
+        if counts[i] > 1:
+            repeated = True
+        
+        i += 1
+    
+    result = not(repeated)
+
+    return result 
